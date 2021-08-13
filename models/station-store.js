2bee1a95112b379412529bfc9cea0d4e6b366c59
Glitch (teal-incandescent-bamboo) <none>
2021-08-13 13:09:07 +0000

    ❣️🛩 Checkpoint ./models/station-store.json:32677130/3850 ./models/station-store.js:32677130/774

--- models/station-store.js
+++ models/station-store.js
@@ -2,54 +2,7 @@
 
 const logger = require('../utils/logger');
 
-const tramore = {
-  title: 'Tramore',
-  readings: [
-    {
-      code: '1',
-      temp:'32',
-      windspeed:'200',
-      pressure:'12'
-    },
-    {
-      code: '2',
-      temp:'32',
-      windspeed:'200',
-      pressure:'12'
-    },
-    {
-      code: '3',
-      temp:'32',
-      windspeed:'200',
-      pressure:'12'
-    },
-  ],
-};
+const stationCollection = require('./station-store.json').stationCollection;
 
-const dunmore = {
-  title: 'Dunmore',
-  readings: [
-    {
-      code: '1',
-      temp:'32',
-      windspeed:'200',
-      pressure:'12'
-    },
-    {
-      code: '2',
-      temp:'32',
-      windspeed:'200',
-      pressure:'12'
-    },
-    {
-      code: '3',
-      temp:'32',
-      windspeed:'200',
-      pressure:'12'
-    },
-  ],
-};
-
-const stationCollection = [tramore, dunmore];
 
 module.exports = stationCollection;
