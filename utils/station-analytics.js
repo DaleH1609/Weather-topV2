6aff2c84df87e3e4600c5b646e1b6e33fab2cb9b
Glitch (teal-incandescent-bamboo) <none>
2021-08-18 18:05:18 +0000

    🧀👲 Checkpoint ./views/dashboard.hbs:32677130/122 ./views/partials/liststations.hbs:32677130/50 ./views/partials/weathercards.hbs:32677130/2 ./utils/station-analytics.js:32677130/905

---
+++ utils/station-analytics.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const stationAnalytics = {
+
+  getLatestTemp(station) {
+    let latestTemp = null;
+    if(station.readings.length > 0){
+       latestTemp = station.readings
+       }
+    
+  }
+};
