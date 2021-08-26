67244c5e8478caab7f5b14f9301fd813f6bffa40
Glitch (teal-incandescent-bamboo) <none>
2021-08-26 21:16:53 +0000

    📡🌄 Checkpoint ./utils/logger.js:32759363/15 ./views/partials/addstation.hbs:32759363/58 ./views/partials/liststations.hbs:32759363/18 ./models/station-store.json:32759363/18 ./controllers/dashboard.js:32759363/25

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -14,7 +14,7 @@ const dashboard = {
       stations: stationStore.getUserStations(loggedInUser.id),
       
     };
-    logger.info('about to render', stationStore.getAllStations());
+    logger.info('about to render', stationStore.getUserStations());
     response.render("dashboard", viewData);
   },
   
@@ -23,7 +23,7 @@ const dashboard = {
     const newStation = {
       id: uuid.v1(),
       userid: loggedInUser.id,
-      name: request.body.name,
+      title: request.body.title,
       longitude: request.body.longitude,
       latitude: request.body.latitude,
       readings: [],
